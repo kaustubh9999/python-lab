b69494ae83952287f4ba3e9e51dd011f29a73d3e
kaustubh9999 <80816514+kaustubh9999@users.noreply.github.com>
2021-03-27 22:12:16 +0530

    Add files via upload

---
+++ find the number of goats and chicken.py
@@ -0,0 +1,6 @@
+#program to find the no. of chicken and goats
+a= int(input("enter the number of heads"))
+b=int(input("enter the no. of feets"))
+x= (b-2*a)/2
+y= a-x
+print(f"the no. of goats are {x} \n the no. of chickens are {y}")
