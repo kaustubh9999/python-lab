5d3b31fb5c70a0b5c08217dc63219c4b468053c8
kaustubh9999 <80816514+kaustubh9999@users.noreply.github.com>
2021-03-27 22:16:58 +0530

    Add files via upload

---
+++ to find the character .py
@@ -0,0 +1,7 @@
+a= eval(input("enter the character"))
+if(a>='A' and a<='Z'):
+    print("upper case character ")
+elif(a>='a' and a<='b'):
+    print("lower case  character")
+elif(a>='0' and a<='9'):
+    print("digits")
